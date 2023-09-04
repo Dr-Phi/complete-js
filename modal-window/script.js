41b0fbdebb3a3e7fd54c1422ceecd54aa1dc2890
Dr-Phi <eliasrojaslon@gmail.com>
2023-09-04 12:04:18 -0500

    modal finished

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,1 +1,29 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnClose = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < 3; i++) {
+  btnsOpenModal[i].addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+btnClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (ev) {
+  if (ev.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
