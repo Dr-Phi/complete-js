3c7cded9290ce09e0727fc2802acc06f88b15bec
Elias Rojas <eliasrojaslon@gmail.com>
2024-02-23 14:24:26 -0500

    ds-op-str coding challenge 1 finished

--- coding-challenges/ds-operators-strings/script.js
+++ coding-challenges/ds-operators-strings/script.js
@@ -38,3 +38,39 @@ const game = {
     team2: 6.5,
   },
 };
+
+// Create one player array for each team
+const [players1, players2] = game.players
+console.log(players1);
+
+/*  For Bayern Munich  create one variable with the goalkeeper's name, and one array  with all the remaining 10 field players */
+const [gk, ...fieldPlayers] = players1
+console.log(gk, fieldPlayers);
+
+// Create an array containing all players of both teams
+const allPlayers = [...players1, ...players2]
+console.log(allPlayers);
+
+/*  During the game, Bayern Munich used 3 substitute players. create a new array containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic' */
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']
+console.log(players1Final);
+
+// Based on the game.odds object, create one variable for each odd
+const {team1, x: draw, team2} = game.odds
+console.log(team1, draw, team2);
+
+/*Write a function  that receives an arbitrary number of player
+names (not an array) and prints each of them, along with the
+number of goals that were scored in total */
+function printGoals(...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich')
+printGoals(...game.scored)
+
+// The team with the lower odd is more likely to win, print it
+
+// team1 < team2 && console.log(game.team1);
+// team1 > team2 && console.log(game.team2);
+console.log(team1 < team2 && game.team1 || game.team2, 'is more likely to win');
