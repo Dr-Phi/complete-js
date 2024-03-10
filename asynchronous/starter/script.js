c091dd8455b46bb83a51c4dbc4d22c1ded8d56b8
Elias Rojas <eliasrojaslon@gmail.com>
2024-03-10 11:03:13 -0500

    Async JS challenge 1 finished

--- asynchronous/starter/script.js
+++ asynchronous/starter/script.js
@@ -8,8 +8,16 @@ const countriesContainer = document.querySelector('.countries');
 // ${data.languages[0].name}
 // ${data.currencies[0].name}
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 const renderCountry = function (data, className = '') {
-    const html = `
+  const html = `
     <article class="country ${className}">
       <img class="country__img" src="${data.flags.png}" />
       <div class="country__data">
@@ -23,27 +31,54 @@ const renderCountry = function (data, className = '') {
       </div>
     </article>
     `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  };
-
-const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then((response)=>response.json())
-    .then(function (data) {
-      console.log(data[0]);
-      renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
-
-      if(!neighbour) return;
-
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        renderCountry(data[0], 'neighbour')
-    })
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 };
 
-getCountryData('colombia')
+const getCountryData = function (country) {
+    // Country 1
+    getJSON(
+      `https://restcountries.com/v3.1/name/${country}`,
+      'Country not found'
+    )
+      .then(data => {
+        console.log(data)
+        renderCountry(data[0]);
+        const neighbour = data[0].borders[0];
+  
+        if (!neighbour) throw new Error('No neighbour found!');
+  
+        // Country 2
+        return getJSON(
+          `https://restcountries.com/v3.1/alpha/${neighbour}`,
+          'Country not found'
+        );
+      })
+      .then(data => renderCountry(data[0], 'neighbour'))
+      .catch(err => {
+        console.error(`${err} 💥💥💥`);
+        renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+      })
+      .finally(() => {
+        countriesContainer.style.opacity = 1;
+      });
+  };
+
+const whereAmI = function (lat, lng) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://us1.locationiq.com/v1/reverse?key=pk.18f47c64fc5d61223cf8dd0ebc19ff94&lat=${lat}&lon=${lng}&format=json`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        const country = data.address.country;
+        console.log(`You are in ${data.address.city}, ${data.address.country}`);
+        resolve(country);
+      })
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
+
+whereAmI(-33.933, 18.474).then(country => getCountryData(country));
